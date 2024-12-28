e6c3dac5aa45c934f83e60765bd4344d2bcca053
George Antonio <george.antonio13@hotmail.com>
2024-12-27 23:24:03 -0300

    Prim refeito

--- Minimum-Tree-Generator/Prim.cpp
+++ Minimum-Tree-Generator/Prim.cpp
@@ -68,7 +68,7 @@ public:
         cout << "Arestas na Árvore Geradora Mínima:\n";
         for (int i = 1; i < V; ++i) {
             if (pai[i] != -1) {
-                cout << pai[i] << " -- " << i << " [Peso: " << chave[i] << "]\n";
+                cout << pai[i] + 1 << " -- " << i + 1 << " [Peso: " << chave[i] << "]\n";
                 pesoTotal += chave[i];
             }
         }
@@ -77,20 +77,33 @@ public:
 };
 
 int main() {
-    // Exemplo de uso
     int V = 12; // Número de vértices
     Grafo g(V);
 
-    // Adicionar arestas ao grafo (baseado nos dados fornecidos)
-    g.adicionarAresta(0, 1, 33);
-    g.adicionarAresta(0, 2, 27);
-    g.adicionarAresta(0, 3, 34);
-    g.adicionarAresta(1, 2, 28);
-    g.adicionarAresta(1, 3, 35);
-    g.adicionarAresta(2, 3, 11);
-    g.adicionarAresta(2, 4, 69);
-    g.adicionarAresta(3, 4, 71);
-    // Adicionar outras arestas conforme necessário...
+    // Dados fornecidos (matriz de adjacência)
+    int matriz[12][12] = {
+        {0, 33, 27, 34, 65, 93, 145, 23, 37, 23, 38, 38},
+        {33, 0, 28, 35, 42, 94, 172, 50, 64, 51, 47, 65},
+        {27, 28, 0, 11, 69, 71, 166, 44, 57, 44, 72, 59},
+        {34, 35, 11, 0, 71, 66, 174, 52, 66, 53, 73, 67},
+        {65, 42, 69, 71, 0, 192, 114, 83, 97, 70, 43, 94},
+        {93, 94, 71, 66, 192, 0, 235, 112, 126, 113, 133, 127},
+        {145, 172, 166, 174, 114, 235, 0, 137, 124, 133, 151, 109},
+        {23, 50, 44, 52, 83, 112, 137, 0, 14, 38, 61, 38},
+        {37, 64, 57, 66, 97, 126, 124, 14, 0, 36, 67, 24},
+        {23, 51, 44, 53, 70, 113, 133, 38, 36, 0, 31, 25},
+        {38, 47, 72, 73, 43, 133, 151, 61, 67, 31, 0, 56},
+        {38, 65, 59, 67, 94, 127, 109, 38, 24, 25, 56, 0}
+    };
+
+    // Adicionar arestas ao grafo
+    for (int i = 0; i < V; i++) {
+        for (int j = i + 1; j < V; j++) {
+            if (matriz[i][j] != 0) {
+                g.adicionarAresta(i, j, matriz[i][j]);
+            }
+        }
+    }
 
     // Executar o algoritmo de Prim
     g.primAGM();
